695736f35bd8863cd2a0e7ce226ec81e82fc8b73
Iván Sánchez Corral <ivan.sanchezc@opendeusto.es>
2021-03-26 15:15:00 +0100

    minor changes

--- README.md
+++ README.md
@@ -1,18 +1,31 @@
 # EcoMonitorServer
 
-## What is this?
+## Description
 
 EcoMonitor server-side backend app, handles API requests and logs data to a MySQL Server.
 
-## Structure
-
-*code structure*
-
-## Features
+### Features
 
 + RESTful API.
 + Token based authorization.
 + OAuth 2.0 compliant. Click *me* for an interactive API demo.
 + *TODO...*
 
-*moar cool stuff... but not yet*
+## Table of Contents
+
+*code structure*
+
+## Installation
+
+1. Any MySQL Server. MariaDB is the recommended choice for compatibility.
+*how to install*
+
+## Usage
+
+1. Do something.
+2. Is this an ordered list?
+3. Yup it definitely is.
+
+## License
+
+*boring legal stuff and boring text*
--- src/sql/create.sql
+++ src/sql/create.sql
@@ -2,11 +2,11 @@
 CREATE DATABASE IF NOT EXISTS ecomonitor;
 USE ecomonitor;
 CREATE TABLE IF NOT EXISTS user
-    (id INT NOT NULL PRIMARY KEY, username VARCHAR(30) NOT NULL UNIQUE, p_hash VARCHAR(255) NOT NULL, installation_id INT DEFAULT 1); /*Add foreign key*/
+    (id MEDIUMINT NOT NULL PRIMARY KEY, username VARCHAR(30) NOT NULL UNIQUE, p_hash VARCHAR(255) NOT NULL); /*Add foreign key*/
 CREATE TABLE IF NOT EXISTS sensor_node
-    (id INT NOT NULL AUTO_INCREMENT, owner_id INT NOT NULL, ip_address VARCHAR(25) NOT NULL, PRIMARY KEY(id, owner_id), FOREIGN KEY(owner_id) REFERENCES user(id));
+    (id MEDIUMINT NOT NULL AUTO_INCREMENT, owner_id INT NOT NULL, ip_address VARCHAR(25) NOT NULL, PRIMARY KEY(id, owner_id), FOREIGN KEY(owner_id) REFERENCES user(id));
 CREATE TABLE IF NOT EXISTS sensor_reading
-    (id INT NOT NULL AUTO_INCREMENT, node_id INT NOT NULL,epoch TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+    (id MEDIUMINT NOT NULL AUTO_INCREMENT, node_id INT NOT NULL,epoch TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
     humidity FLOAT(3,2), temperature FLOAT(3,2), PRIMARY KEY(id, node_id, epoch)); /*FOREIGN KEY(node_id) REFERENCES sensor_node(id)*/
 CREATE USER IF NOT EXISTS 'ecomonitor'@'localhost' IDENTIFIED BY 'ecoecomonitor';
 GRANT ALL PRIVILEGES on ecomonitor.* TO 'ecomonitor'@'localhost';
